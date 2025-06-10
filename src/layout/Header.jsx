bb023b783595ad10f6d431a75e0bbc285b7e957f
salih-sengul <salih.sengul@outlook.com>
2025-06-11 01:55:09 +0300

    Mobile Header
    Mobile Header bitti. Hamburger butona basınca menu açılıyor. tailwind eksikleri eklendi. bir kaç tailwind rengi eklendi. font eklendi. Bazı tag stilleri tanımlandı.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,19 +1,37 @@
-import { Camera, User } from 'lucide-react'
-import React from 'react'
+import { Camera, Menu, Search, ShoppingCart, User } from 'lucide-react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <div>
-      <div>
-        <p>Bandage</p>
-        <div>
+    <div className='p-9'>
+      <div className='flex justify-between'>
+        <h3>Bandage</h3>
+        <div className='flex gap-6 items-center'>
           <div>
-            <Camera color="red" size={48} />
+            <User />
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <Search />
+          </div>
+          <div>
+            <ShoppingCart />
+          </div>
+          <div onClick={()=>setMenuOpen((prev)=>!prev)} >
+            <Menu />
+          </div>
         </div>
+      </div>
+      <div className={`text-second-text-color ${menuOpen?"flex":"hidden"} justify-center py-24 text-3xl transition-all`}>
+        <nav >
+          <ul className='flex flex-col gap-8'>
+            <li>Home</li>
+            <li>Product</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </div>
     </div>
   )
