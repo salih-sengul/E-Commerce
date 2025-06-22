b68944a914424a6f56f9b28c8d2382e8215bd166
salih-sengul <salih.sengul@outlook.com>
2025-06-23 02:14:59 +0300

    slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -16,20 +16,23 @@ function Slider({ className }) {
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index}>
-            <div className="text-light-text-color relative flex h-[753px] flex-col items-center justify-center gap-12 px-13 text-center lg:items-start lg:pl-58 lg:text-left">
+            <div className="text-light-text-color relative flex h-[753px] items-center justify-center px-13">
               <div className="absolute top-0 left-0 z-[-1] h-full w-full bg-[url('assets/slider-p1.png')] bg-cover bg-center"></div>
-              <div className="h5">SUMMER 2020</div>
-              <div className="h2 lg:h1">NEW COLLECTION</div>
-              <div className="h4 text-primary-foreground lg:max-w-[369px]">
-                We know how large objects will act, but things on a small scale.
+              <div className="flex w-full max-w-[1050px] flex-col items-center justify-center gap-12 text-center lg:items-start lg:text-left">
+                <div className="h5">SUMMER 2020</div>
+                <div className="h2 lg:text-[58px]">NEW COLLECTION</div>
+                <div className="h4 text-primary-foreground lg:max-w-[369px]">
+                  We know how large objects will act, but things on a small
+                  scale.
+                </div>
+                <Button
+                  variant={"success"}
+                  size={"lg"}
+                  className="h3 bg-success-color text-light-text-color rounded-b-none"
+                >
+                  SHOP NOW
+                </Button>
               </div>
-              <Button
-                variant={"success"}
-                size={"lg"}
-                className="h3 bg-success-color text-light-text-color rounded-b-none"
-              >
-                SHOP NOW
-              </Button>
             </div>
           </CarouselItem>
         ))}
